5ad468bc67e5e4c61fb76ed90a857b2301b6a476
sareenarazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-09 01:05:41 -0500

    Write ppm data to file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-fn main() {
+use std::fs::File;
+use std::io::{self, Write};
+
+fn main() -> std::io::Result<()>{
+    let mut image = File::create("img.ppm")?;
+
     let width = 256;
     let height = 256;
 
-
-    println!("P3\n{width} {height}\n255");
+    image.write_all(format!("P3\n{width} {height}\n255\n").as_bytes())?;
 
     for j in 0..height {
+
+        eprint!("\rScan lines remaining: {} ", height - j);
+        io::stderr().flush()?;
+
         for i in 0..width {
             let r: f64 = i as f64 / (width as f64 - 1.0);
             let g: f64 = j as f64 / (height as f64 - 1.0);
@@ -15,9 +23,12 @@ fn main() {
             let ig: usize = (255.999 * g) as usize;
             let ib: usize = (255.999 * b) as usize;
 
-            println!("{ir} {ig} {ib}");
+            image.write_all(format!("{ir} {ig} {ib}\n").as_bytes())?;
 
         }
     }
+
+    eprint!("\rDone.                 \n");
+    Ok(())
 
 }
